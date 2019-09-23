a0c781b6b40be65660f945c0c481d82b8a287b3d
simonchen92 <simon.k.chen92@gmail.com>
2019-09-23 14:13:46 -0400

    Higher Order Function practice on Code War

--- code-practice/codewars/Higher-Order-Function-5.js
+++ code-practice/codewars/Higher-Order-Function-5.js
@@ -4,12 +4,12 @@
 
 // For example, given the following input array:
 
-// const list1 = [
-//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
-//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
-//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
-//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' }
-// ]
+const list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' }
+]
 
 // your function should return the following object (associative array in PHP):
 

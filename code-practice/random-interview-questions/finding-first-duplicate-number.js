cb1c8e59c68b24d24847fb7aa961e0055e91dc20
simonchen92 <simon.k.chen92@gmail.com>
2020-02-26 14:22:37 -0500

    Fixes to solution to find first duplicate

--- code-practice/random-interview-questions/finding-first-duplicate-number.js
+++ code-practice/random-interview-questions/finding-first-duplicate-number.js
@@ -2,9 +2,11 @@
 // find first reoccuring duplicate
 
 const array = [3, 5, 6, 8, 5, 3] // =>  Answer should be 5
+const array1 = [1, 3, 4, 2, 1, 3] // => Answer should be 1
 
-const firstDupe = array.find(n => {
-  return array.indexOf(n)
-})
+const firstDupe = input => {
+  return input.find((num, index) => input.indexOf(num) !== index)
+}
 
-console.log(firstDupe)
+console.log(firstDupe(array))
+console.log(firstDupe(array1))

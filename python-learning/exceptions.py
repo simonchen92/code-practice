bc29a8a92f708cfed681b1109c228a40c063cadc
simonchen92 <simon.k.chen92@gmail.com>
2020-04-06 15:22:09 -0400

    Removed python folder
    Moved folder to a different repository for strictly python related code

--- python-learning/exceptions.py
+++
@@ -1,9 +0,0 @@
-# this is the basic concept I learn about exceptions
-
-try:
-    age = int(input('Age: '))
-    print(age)
-except ZeroDivisionError:
-    print('Age cannot be 0!')
-except ValueError:
-    print('Invalid Value. Please try again!')

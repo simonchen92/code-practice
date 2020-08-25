ea789b4d9f0c5f3623d4f197a46c712c59a2a62b
simonchen92 <simon.k.chen92@gmail.com>
2020-08-25 16:02:29 -0400

    Fixed console logs

--- code-practice/leetcode/shuffle-the-array.js
+++ code-practice/leetcode/shuffle-the-array.js
@@ -30,14 +30,12 @@ https://leetcode.com/problems/shuffle-the-array/
 let shuffle = function (nums, n) {
   let newShuffleArr = [];
   for (let i = 0; i < n; i++) {
-    console.log('nums[i] is:', nums[i]);
     newShuffleArr.push(nums[i]);
-    console.log('nums[i+n] value is:', nums[i + n]);
     newShuffleArr.push(nums[i + n]);
   }
   return newShuffleArr;
 };
 
 console.log(shuffle([2, 5, 1, 3, 4, 7], 3)); // [2,3,5,4,1,7]
-// console.log(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)); // [1,4,2,3,3,2,4,1]
-// console.log(shuffle([1, 1, 2, 2], 2)); // [1,2,1,2]
+console.log(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)); // [1,4,2,3,3,2,4,1]
+console.log(shuffle([1, 1, 2, 2], 2)); // [1,2,1,2]
